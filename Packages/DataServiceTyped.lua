8b10e3a3557e7d58345e58faa289fbba7c1a11b5
ii6ii <ivansalaginov97@gmail.com>
2026-09-15 17:38:51 +0300

    Project Created

---
+++ Packages/DataServiceTyped.lua
@@ -0,0 +1,8 @@
+local REQUIRED_MODULE = require(script.Parent._Index["leifstout_dataservicetyped@1.0.5"]["dataservicetyped"])
+export type Client<T> = REQUIRED_MODULE.Client<T>
+export type Server<T> = REQUIRED_MODULE.Server<T>
+export type DataOptions<T> = REQUIRED_MODULE.DataOptions<T>
+export type Value<T> = REQUIRED_MODULE.Value<T>
+export type PublicDataServiceServer<T> = REQUIRED_MODULE.PublicDataServiceServer<T>
+export type ServerPlayerData<T> = REQUIRED_MODULE.ServerPlayerData<T>
+return REQUIRED_MODULE
